5e52307d5437e9584c814a4f3e4c64d95adc9715
Andrew Tirto K <andrewtirtokusumo@gmail.com>
2017-05-15 15:02:15 +0700

    add files via upload
    coba nambah program

---
+++ KalkulatorMini/KalkulatorMini/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KalkulatorMini
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_plus_Click(object sender, EventArgs e)
+        {
+            int operand1, operand2;
+            //int operand1 = int.Parse(textBox_operand1.Text);
+            int.TryParse(textBox_operand1.Text, out operand1);
+            int.TryParse(textBox_operand2.Text, out operand2);
+            //int operand2 = int.Parse(textBox_operand2.Text);
+            int total = operand1+operand2;
+            textBox_answer.Text = total.ToString();
+        }
+
+        private void button_minus_Click(object sender, EventArgs e)
+        {
+            int operand1, operand2;
+            //int operand1 = int.Parse(textBox_operand1.Text);
+            int.TryParse(textBox_operand1.Text, out operand1);
+            int.TryParse(textBox_operand2.Text, out operand2);
+            //int operand2 = int.Parse(textBox_operand2.Text);
+            int total = operand1 - operand2;
+            textBox_answer.Text = total.ToString();
+        }
+
+        private void button_multiply_Click(object sender, EventArgs e)
+        {
+            int operand1, operand2;
+            //int operand1 = int.Parse(textBox_operand1.Text);
+            int.TryParse(textBox_operand1.Text, out operand1);
+            int.TryParse(textBox_operand2.Text, out operand2);
+            //int operand2 = int.Parse(textBox_operand2.Text);
+            int total = operand1 * operand2;
+            textBox_answer.Text = total.ToString();
+        }
+
+        private void button_divider_Click(object sender, EventArgs e)
+        {
+            double operand1, operand2;
+            //int operand1 = int.Parse(textBox_operand1.Text);
+            double.TryParse(textBox_operand1.Text, out operand1);
+            double.TryParse(textBox_operand2.Text, out operand2);
+            //int operand2 = int.Parse(textBox_operand2.Text);
+            double total = operand1 / operand2;
+            textBox_answer.Text = total.ToString();
+        }
+
+
+        
+    }
+}
