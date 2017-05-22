704965d51fd26d50eca4c86c41a678fccb2917f0
Andrew Tirto K <andrewtirtokusumo@gmail.com>
2017-05-22 22:35:54 +0700

    nambah login
    login

---
+++ FormLogin.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace SIMPEG
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistem Informasi Manajemen Kepegawaian";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password";
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(164, 72);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(138, 20);
+            this.tb_username.TabIndex = 4;
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(164, 106);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(138, 20);
+            this.tb_password.TabIndex = 5;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(153, 163);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 6;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 214);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Button btn_login;
+    }
+}
---
+++ FormLogin.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace SIMPEG
+{
+    public partial class FormLogin : Form
+    {
+        string MyConnectionString = "Server=vps.eriksenfinarto.com;Port=3306;Database=pemvis_simpeg;Uid=pemvis_simpeg;Pwd=pemvis2017";
+
+        public FormLogin()
+        {
+            InitializeComponent();
+            
+        }
+
+        private bool validate_login(string username, string password)
+        {
+            MySqlConnection connection = new MySqlConnection(MyConnectionString);
+            connection.Open();
+            try
+            {
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT * FROM pegawai WHERE email = @email AND password = @password";
+                cmd.Parameters.AddWithValue("@email", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.ExecuteNonQuery();
+                cmd.Connection = connection;
+                MySqlDataReader login = cmd.ExecuteReader();
+                if (login.Read())
+                {
+                    connection.Close();
+                    return true;
+                }
+                else
+                {
+                    connection.Close();
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string username = tb_username.Text;
+            string password = tb_password.Text;
+            bool r = validate_login(username, password);
+            if (r)
+                MessageBox.Show("Correct Login Credentials");
+            else
+                MessageBox.Show("Incorrect Login Credentials");
+            }
+            
+        }
+    }
+
